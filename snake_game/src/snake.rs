bb50d8525044fc8ff2071f74acab5a2425d675cc
jinseok9338 <jinseok9338@gmail.com>
2021-11-14 09:53:03 +0000

    Making snake game

---
+++ snake_game/src/snake.rs
@@ -0,0 +1,40 @@
+use std::collections::LinkedList;
+use piston_window::{Context, G2d};
+use piston_window::types::Color;
+
+use crate::draw::draw_block;
+
+const SNAKE_COLOR: Color = [0.00, 1.80, 0.00, 1.0];
+#[derive(Copy, Clone, PartialEq)]
+pub enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
+impl Direction {
+    // To prevent the snake from going backward since that is illegal move 
+    pub fn opposite(&self) -> Direction {
+        match *self {
+            Direction::Up => Direction::Down,
+            Direction::Down => Direction::Up,
+            Direction::Left => Direction::Right,
+            Direction::Right => Direction::Left,
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+struct Block {
+    x: i32,
+    y: i32,
+}
+
+pub struct Snake {
+    direction: Direction,
+    body: LinkedList<Block>,
+    tail: Option<Block>,
+}
+
+

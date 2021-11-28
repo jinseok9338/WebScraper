ba28c6aaa6a7ea088caa5def282165dbbf732a9e
jinseok9338 <jinseok9338@gmail.com>
2021-11-28 23:56:42 +0000

    Make Json as a return type using foramat

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,28 +1,25 @@
 #[macro_use] extern crate rocket;
 
-use rocket::http::{ContentType, Status};
+
 use tokio::task::spawn_blocking;
-use yahoo::YResponse;
 use yahoo_finance_api as yahoo;
 use chrono::{Utc,TimeZone};
-use rocket::serde::{Serialize, json::Json};
+use rocket::response::content;
 
-#[derive(Serialize)]
-struct APIResponse { 
-    res: YResponse
- }
+
 
 
 #[get("/")]
-async fn index() -> Json<APIResponse> {
+async fn index() -> content::Json<String> {
     let provider = yahoo::YahooConnector::new();
     let start = Utc.ymd(2020, 1, 1).and_hms_milli(0, 0, 0, 0);
     let end = Utc.ymd(2020, 1, 31).and_hms_milli(23, 59, 59, 999);
     // returns historic quotes with daily interval
     let resp = spawn_blocking(move || {provider.get_quote_history("AAPL", start, end)}).await;
     let quotes = resp.unwrap().unwrap();
+    let formatted_quotes = format!("{:?}", quotes);
     println!("Apple's quotes in January: {:?}", quotes);
-    Json(APIResponse{ res:quotes});
+    content::Json(formatted_quotes)
 }
 
 

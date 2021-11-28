d00c620afa1d95571b49e03105fc8980df107bdd
jinseok9338 <jinseok9338@gmail.com>
2021-11-28 14:14:42 +0000

    Bugs to fix...

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,21 +1,30 @@
 #[macro_use] extern crate rocket;
 
+use rocket::http::{ContentType, Status};
+use tokio::task::spawn_blocking;
+use yahoo::YResponse;
 use yahoo_finance_api as yahoo;
 use chrono::{Utc,TimeZone};
+use rocket::serde::{Serialize, json::Json};
+
+
+
 
 #[get("/")]
-fn index() {
+async fn index() -> Json<Response> {
     let provider = yahoo::YahooConnector::new();
     let start = Utc.ymd(2020, 1, 1).and_hms_milli(0, 0, 0, 0);
     let end = Utc.ymd(2020, 1, 31).and_hms_milli(23, 59, 59, 999);
     // returns historic quotes with daily interval
-    let resp =provider.get_quote_history("AAPL", start, end).unwrap();
-    let quotes = resp.quotes().unwrap();
+    let resp = spawn_blocking(move || {provider.get_quote_history("AAPL", start, end)}).await;
+    let quotes = resp.unwrap().unwrap();
     println!("Apple's quotes in January: {:?}", quotes);
+    return json!(quotes)
+    
 }
 
 
 #[launch]
 fn rocket() -> _ {
     rocket::build().mount("/", routes![index])
-}
+} 

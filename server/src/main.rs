04e157b30c001e4a1e2cc018d41e01ea211a533c
jinseok9338 <jinseok9338@gmail.com>
2021-11-28 14:17:34 +0000

    changes

--- server/src/main.rs
+++ server/src/main.rs
@@ -7,11 +7,14 @@ use yahoo_finance_api as yahoo;
 use chrono::{Utc,TimeZone};
 use rocket::serde::{Serialize, json::Json};
 
-
+#[derive(Serialize)]
+struct APIResponse { 
+    res: YResponse
+ }
 
 
 #[get("/")]
-async fn index() -> Json<Response> {
+async fn index() -> Json<APIResponse> {
     let provider = yahoo::YahooConnector::new();
     let start = Utc.ymd(2020, 1, 1).and_hms_milli(0, 0, 0, 0);
     let end = Utc.ymd(2020, 1, 31).and_hms_milli(23, 59, 59, 999);
@@ -19,8 +22,7 @@ async fn index() -> Json<Response> {
     let resp = spawn_blocking(move || {provider.get_quote_history("AAPL", start, end)}).await;
     let quotes = resp.unwrap().unwrap();
     println!("Apple's quotes in January: {:?}", quotes);
-    return json!(quotes)
-    
+    Json(APIResponse{ res:quotes});
 }
 
 

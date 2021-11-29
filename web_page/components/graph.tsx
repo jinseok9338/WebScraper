568995bc50d36f4039a44dde9cdda8cc0de3eb0f
jinseok9338 <jinseok9338@gmail.com>
2021-11-29 01:01:01 +0000

    Cors Problem...

--- web_page/components/graph.tsx
+++ web_page/components/graph.tsx
@@ -1,7 +1,31 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 interface GraphForThingsProps {
-  gridArea: String;
+  gridArea: string;
 }
+
 const GraphForThings = ({ gridArea }: GraphForThingsProps) => {
+  const [res, setRes] = useState([]);
+
+  useEffect(() => {
+    const getResult = async (url: string) => {
+      try {
+        const res = await axios.get(url, {
+          headers: {
+            "Content-Type": "application/",
+            "Access-Control-Allow-Origin": "*",
+          },
+        });
+        console.log(res);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+
+    getResult("https://8000-magenta-ape-b2e7tsx7.ws-us17.gitpod.io/");
+  }, []);
+
   return (
     <div className={`h-full w-full ${gridArea} bg-red-400`}>Hello World</div>
   );
